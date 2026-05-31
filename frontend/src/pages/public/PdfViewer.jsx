3351f6d44d30179e75d52a5c5b22e2dc9dbf272f
PBJT-Finger <demiosa2555@gmail.com>
2026-05-31 14:47:26 +0700

    fix(frontend): use .js worker instead of .mjs for better ios safari compatibility

--- frontend/src/pages/public/PdfViewer.jsx
+++ frontend/src/pages/public/PdfViewer.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const PdfViewer = () => {
   const { id } = useParams();
